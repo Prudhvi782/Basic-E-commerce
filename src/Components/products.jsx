077495065d332126ad1448d86761ef9df63eb5d3
Prudhvi782 <prudhvikalakonda@gmail.com>
2026-05-27 12:14:27 +0530

    Fix audio issue

--- src/Components/products.jsx
+++ src/Components/products.jsx
@@ -6,16 +6,16 @@ export let products = [
     imgPath:["https://www.mobex.in/cdn/shop/files/61cwywLZR-L._SX679.jpg?v=1739798195&width=1214",
         "https://inspireonline.in/cdn/shop/files/iPhone_14_Blue_PDP_Image_Position-2__WWEN.jpg?v=1691420929&width=823"
     ]},
-    {id:2 , name:"iphone16promax" , price:100000 , category:"Mobile" ,audio:"public/audio/vikram_bgm.mp3", imgPath:["https://cdn.jiostore.online/v2/jmd-asp/jdprod/wrkr/products/pictures/item/free/original/apple/494423061/0/qwsjtsaGaO-dfb6bpF93_-Apple-iPhone16ProMax-494423061-i-1-1200Wx1200H.jpeg",
+    {id:2 , name:"iphone16promax" , price:100000 , category:"Mobile" ,audio:"/audio/vikram_bgm.mp3", imgPath:["https://cdn.jiostore.online/v2/jmd-asp/jdprod/wrkr/products/pictures/item/free/original/apple/494423061/0/qwsjtsaGaO-dfb6bpF93_-Apple-iPhone16ProMax-494423061-i-1-1200Wx1200H.jpeg",
                                                                                     "https://crdms.images.consumerreports.org/prod/products/cr/models/415497-smartphones-apple-iphone-16-pro-max-256gb-10041053.png"
     ]},
-    {id:3 , name:"Samsung" , price:35000 , category:"Mobile",audio:"public/audio/rolex.mp3", imgPath:["https://www.myg.in/images/thumbnails/300/300/detailed/73/1_03pb-vk-removebg-preview.png.png",
+    {id:3 , name:"Samsung" , price:35000 , category:"Mobile",audio:"/audio/rolex.mp3", imgPath:["https://www.myg.in/images/thumbnails/300/300/detailed/73/1_03pb-vk-removebg-preview.png.png",
                                                                             "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT4Fp5oXbzA8IlKjMXaHP0G7YJ6mwwG4Yke6w&s"
     ]},
-    {id:4 , name:"Asus Vivobook" , price:65000 , category:"Laptop" ,audio:"public/audio/master_title_bgm.mp3", imgPath:["https://supremeindia.com/cdn/shop/files/01_800e4d2d-539a-4165-8e1c-61d1a0af9deb.png?v=1753781114&width=1445",
+    {id:4 , name:"Asus Vivobook" , price:65000 , category:"Laptop" ,audio:"/audio/master_title_bgm.mp3", imgPath:["https://supremeindia.com/cdn/shop/files/01_800e4d2d-539a-4165-8e1c-61d1a0af9deb.png?v=1753781114&width=1445",
                                                                                 "https://in.store.asus.com/media/catalog/product/x/1/x1605zac_transperant_silver_fngrprnt_bcklt_kbd_3__3.jpg"
     ]},
-    {id:5 , name:"Sony speaker" , price:2000 , category:"Speaker" ,audio:"public/audio/og_ringtone.mp3", imgPath:["https://www.sony.co.in/image/bb9eb0ba7cd3fdabdcdbab8eff237bf5?fmt=png-alpha&wid=1578&hei=1050&bgcolor=F6F9FF",
+    {id:5 , name:"Sony speaker" , price:2000 , category:"Speaker" ,audio:"/audio/og_ringtone.mp3", imgPath:["https://www.sony.co.in/image/bb9eb0ba7cd3fdabdcdbab8eff237bf5?fmt=png-alpha&wid=1578&hei=1050&bgcolor=F6F9FF",
                                                                                 "https://m.media-amazon.com/images/I/71speTdvY9L._AC_UF1000,1000_QL80_.jpg"
     ]}
 
